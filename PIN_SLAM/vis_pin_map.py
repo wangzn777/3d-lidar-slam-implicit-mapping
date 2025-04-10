d505cd80ae592c587c2ec4763503c10a3ec5c0cb
wang.zy <wzy9393@gmail.com>
2025-04-11 01:41:55 +0200

    pytorch 2.6.x will change the value of weights_only from false to true as default value

--- PIN_SLAM/vis_pin_map.py
+++ PIN_SLAM/vis_pin_map.py
@@ -26,7 +26,7 @@ from gui.gui_utils import ParamsGUI, VisPacket
 
 
 '''
-    load the pin-map and do the reconstruction 
+    load the pin-map and do the reconstruction
 '''
 
 app = typer.Typer(add_completion=False, rich_markup_mode="rich", context_settings={"help_option_names": ["-h", "--help"]})
@@ -69,12 +69,12 @@ def vis_pin_map(
     run_path = setup_experiment(config, sys.argv, debug_mode=True)
 
     mp.set_start_method("spawn") # don't forget this
-    
+
     # initialize the mlp decoder
     geo_mlp = Decoder(config, config.geo_mlp_hidden_dim, config.geo_mlp_level, 1)
     sem_mlp = Decoder(config, config.sem_mlp_hidden_dim, config.sem_mlp_level, config.sem_class_count + 1) if config.semantic_on else None
     color_mlp = Decoder(config, config.color_mlp_hidden_dim, config.color_mlp_level, config.color_channel) if config.color_on else None
-    
+
     mlp_dict = {}
     mlp_dict["sdf"] = geo_mlp
     mlp_dict["semantic"] = sem_mlp
@@ -84,40 +84,43 @@ def vis_pin_map(
     neural_points: NeuralPoints = NeuralPoints(config)
 
     # Load the map
-    loaded_model = torch.load(config.model_path)
+    loaded_model = torch.load(config.model_path, weights_only=False)
     neural_points = loaded_model["neural_points"]
-    load_decoders(loaded_model, mlp_dict) 
+    load_decoders(loaded_model, mlp_dict)
     neural_points.temporal_local_map_on = False
-    neural_points.recreate_hash(neural_points.neural_points[0], torch.eye(3).cuda(), False, False)
+    if torch.cuda.is_available():
+        neural_points.recreate_hash(neural_points.neural_points[0], torch.eye(3).cuda(), False, False)
+    else:
+        neural_points.recreate_hash(neural_points.neural_points[0], torch.eye(3).cpu(), False, False)
     neural_points.compute_feature_principle_components(down_rate = 59)
     print("PIN Map loaded")
 
     # mesh reconstructor
     mesher = Mesher(config, neural_points, mlp_dict)
-    
-    mesh_on = (mesh_res_m is not None)  
+
+    mesh_on = (mesh_res_m is not None)
     if mesh_on:
         config.mc_res_m = mesh_res_m
         config.mesh_min_nn = mc_nn
-    
+
     q_main2vis = q_vis2main = None
     if o3d_vis_on:
         # communicator between the processes
-        q_main2vis = mp.Queue() 
+        q_main2vis = mp.Queue()
         q_vis2main = mp.Queue()
 
         params_gui = ParamsGUI(
             q_main2vis=q_main2vis,
             q_vis2main=q_vis2main,
             config=config,
-            local_map_default_on=False, 
+            local_map_default_on=False,
             mesh_default_on=mesh_on,
             neural_point_map_default_on=config.neural_point_map_default_on,
         )
         gui_process = mp.Process(target=slam_gui.run, args=(params_gui,))
         gui_process.start()
         time.sleep(3) # second
-    
+
     cur_mesh = None
     out_mesh_path = None
 
@@ -126,12 +129,12 @@ def vis_pin_map(
         if os.path.exists(cropped_ply_path):
             cropped_pc = o3d.io.read_point_cloud(cropped_ply_path)
             print("Load region for meshing from {}".format(cropped_ply_path))
-        
+
         else:
             cropped_pc = neural_points.get_neural_points_o3d(query_global=True, random_down_ratio=23)
-    
+
         mesh_aabb = cropped_pc.get_axis_aligned_bounding_box()
-        chunks_aabb = split_chunks(cropped_pc, mesh_aabb, mesh_res_m*300) 
+        chunks_aabb = split_chunks(cropped_pc, mesh_aabb, mesh_res_m*300)
 
         if output_mesh_filename is not None:
             out_mesh_path = os.path.join(result_folder, "mesh", output_mesh_filename)
@@ -139,10 +142,10 @@ def vis_pin_map(
 
         mc_cm_str = str(round(mesh_res_m*1e2))
         print("Reconstructing the mesh with resolution {} cm".format(mc_cm_str))
-        cur_mesh = mesher.recon_aabb_collections_mesh(chunks_aabb, mesh_res_m, out_mesh_path, False, config.semantic_on, 
+        cur_mesh = mesher.recon_aabb_collections_mesh(chunks_aabb, mesh_res_m, out_mesh_path, False, config.semantic_on,
                                                       config.color_on, filter_isolated_mesh=True, mesh_min_nn=mc_nn)
         print("Reconstructing the global mesh done")
-    
+
     remove_gpu_cache()
 
     if o3d_vis_on:
@@ -161,8 +164,8 @@ def vis_pin_map(
                 cur_mesh = None
 
             q_main2vis.put(packet_to_vis)
-            time.sleep(1.0) 
-            
-    
+            time.sleep(1.0)
+
+
 if __name__ == "__main__":
     app()
