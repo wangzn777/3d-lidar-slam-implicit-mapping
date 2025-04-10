42bd585a1c7754d1684d19555445b618bb50e396
wang.zy <wzy9393@gmail.com>
2025-04-11 01:43:42 +0200

    if the downloaded file exsits, then will direct extract

--- PIN_SLAM/scripts/download_replica.sh
+++ PIN_SLAM/scripts/download_replica.sh
@@ -6,7 +6,7 @@ cd data
 # you can also download the Replica.zip manually through
 # provided by iMap and Nice-SLAM
 echo Downloading Replica dataset...
-wget https://cvg-data.inf.ethz.ch/nice-slam/data/Replica.zip
+! [[ -e Replica.zip ]] && wget https://cvg-data.inf.ethz.ch/nice-slam/data/Replica.zip
 
 echo Extracting dataset...
 unzip Replica.zip
